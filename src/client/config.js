689b06569b74af229f85c6b78a5069cf7ab6b108
Mohammad Sweiti <mohammadsweity57669@gmail.com>
2025-03-12 02:15:04 +0200

    Done

--- src/client/config.js
+++ src/client/config.js
@@ -1,3 +1,3 @@
-export const GEO_USERNAME = 'menoo20';
-export const WEATHER_API_KEY = 'f30edf494a814d6bad7b15354c8111e2';
-export const PIXABAY_API_KEY = '24559697-9027e06b209242a7359757c40';
+export const GEO_USERNAME = 'mohammad';
+export const WEATHER_API_KEY = 'dADvEapvIBge89dQcNFxp';
+export const PIXABAY_API_KEY = 'p1FJ5TaOLVky3kXkdCrHA9gocUHI5IDQls44lmmR';
